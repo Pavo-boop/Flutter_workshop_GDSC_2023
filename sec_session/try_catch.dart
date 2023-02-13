85960a5a1e3d3eef0da2c9aa89cf69ab137a1493
Pavo-boop <118069754+Pavo-boop@users.noreply.github.com>
2023-02-13 13:11:19 +0200

    messge

--- sec_session/try_catch.dart
+++ sec_session/try_catch.dart
@@ -1,17 +1,30 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  int x = 4;
-  int a = 0;
-  int result;
+  int? x = 4;
+  int? a = 0;
+  int? result;
+    if(result == int)
+  {
+    print(result);
+  }else{
+    throw Exception('maybe result = non-integer value or empty');
+  }
+  try{
+    print(result);
+  } catch(e){
+    print('there is an expeption');
+  }
+  return print('thank you');
   // طريقة عشان اخلي الايرور بدل ما يبوظلي البروجيكت، اخليه يطبعلي ان فيه مشكلة
 
   // try - catch - on - finally
   try {
     result = x ~/ a;
   } on UnsupportedError catch (error) {
-    // هنا بحدد نوع الايرور دا لو جه المفروض الكومبايلر يعمل اية
-    print('you cant divide by zero!: $error');
+    if(a  == 0){
+      print('you cant divide by zero!: $error');
+    }// هنا بحدد نوع الايرور دا لو جه المفروض الكومبايلر يعمل اية
   } finally {
     print('reach to finally code');
   }
@@ -19,7 +32,7 @@ void main(List<String> args) {
   // FormatException
   try {
     if (x != 10) {
-      throw FormatException();
+      throw FormatException('Value must be not equal 10 ');
     }
   } on FormatException catch (error) {
     print('FormatException: $error');
@@ -29,8 +42,10 @@ void main(List<String> args) {
   String ticketNumber = "342531231234";
   try {
     if (ticketNumber[0] != '1') {
-      throw CustomException();
-    }
+      throw Exception("first number must be 3 ");
+    } else if (ticketNumber.length != 12){
+      throw new Exception("ticket number lenght must equal = 12");
+    };
   } on CustomException catch (e) {
     print(e.errorString());
   } on StartWith1 catch (e) {
@@ -40,6 +55,11 @@ void main(List<String> args) {
   try {
     StartWith1.testNumber(a);
     int divide = x ~/ a;
+    if(x<a){
+      throw Exception('value of x must be grater than a');
+    }if(a == double){
+      throw Exception('a musst to equal integer value');
+    };
   } on StartWith1 catch (e) {
     print(e.errorMessage());
   }
